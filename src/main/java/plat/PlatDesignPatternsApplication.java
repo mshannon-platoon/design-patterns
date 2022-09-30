0ffb9816b22615dcd9bcb8200651b81d69e70693
mshannon-platoon <platoon.development@gmail.com>
2022-09-30 11:17:40 +0100

    initial commit

---
+++ src/main/java/plat/PlatDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package plat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatDesignPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatDesignPatternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/plat/PlatDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package plat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatDesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
